52b858bad1e2fa1db7856f8fa6041b1ad841f0d0
Jihyuk Lee <lead@sver.dev>
2024-10-28 20:41:21 +0900

    fix :: 외부 패키지를 불러오지 못하던 문제 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,12 @@ repositories {
     mavenCentral()
 }
 
+tasks.jar {
+    manifest { attributes["Main-Class"] = "org.jihyuklab.discordbot.MainKt" }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
 dependencies {
     testImplementation(kotlin("test"))
     implementation("net.dv8tion:JDA:5.1.2")
